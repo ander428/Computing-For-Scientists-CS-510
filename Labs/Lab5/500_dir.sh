6e1ffd8bfcce6d7de522bb866a0dd7a6811d3a09
ander428 <ander428@mail.chapman.edu>
2020-10-02 11:23:26 -0700

    assignment 5 pt 1

---
+++ Labs/Lab5/500_dir.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in `seq 1 500`; do
+    mkdir "./dir_${i}/";
+    echo "line 1" >> ./dir_${i}/test.txt
+    echo "line 2" >> ./dir_${i}/test.txt
+    echo "line 3" >> ./dir_${i}/test.txt
+    echo "line 4" >> ./dir_${i}/test.txt
+    echo "line 5" >> ./dir_${i}/test.txt
+done
